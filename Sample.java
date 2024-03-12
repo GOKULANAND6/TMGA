3fe1ceba55050e93f8f3fb14bf0a364566359522
GOKULANAND6 <160696205+GOKULANAND6@users.noreply.github.com>
2024-03-12 09:57:05 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -3,6 +3,7 @@ class Sample
     public static void main(String[] args)
     {
       System.out.println("Hello World");
+      System.out.println("Hai Relevantians");
       System.out.println("New Line from Branch1");
     }
   }
